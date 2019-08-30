f25de2a9f71316c5a25b47058912a23893a891d1
yo_i <yo_i@orc-net.com>
2019-08-30 17:15:11 +0900

    commit

---
+++ simpleTube/SUEntity.swift
@@ -0,0 +1,88 @@
+//
+//  SUEntity.swift
+//  simpleTube
+//
+//  Created by yo_i on 2019/08/30.
+//  Copyright © 2019年 yang. All rights reserved.
+//
+
+import Foundation
+
+let GoogleApiKey:String = "AIzaSyCr58GUWUuFoQ2ROgqLWe4i50FLkAqkwVg"
+let GoogleApiBaseUrl:String = "https://www.googleapis.com/youtube/v3/"
+
+
+/// エンティティベース
+class YTEntityBase:NSObject
+{
+    var entitySelected:Bool = false
+    var controlInfo:Dictionary<String,String> = [:]
+    /// エンティティ中のパラメータの初期化が必須
+    required override init(){}
+    
+    /// ディプコッピー、同じクラスのインターフェイスを作成
+    ///
+    /// - Returns: 自分のコッピー
+    override func copy() -> Any
+    {
+        let newInstase = type(of: self).init()
+        return newInstase
+    }
+    
+    
+    /// 該当エンティティはweb通信のパラメータとして使う場合、実装する
+    ///
+    /// - Returns: 通信パラメータのハッシュテーブル
+    func getParamToDictionary()->FFDataSetDictionary
+    {
+        //デフォルトパラメータとサービスのパラメータ名一致することを想定している、じゃない場合、手動でkeyとvalueをセットするが必要です
+        var resultDic = FFDataSetDictionary()
+        let mirror = Mirror.init(reflecting: self)
+        for m in mirror.children
+        {
+            guard let key = m.label else
+            {
+                continue
+            }
+            if m.value is String
+            {
+                resultDic[key] = m.value
+            }
+            else if m.value is [String]
+            {
+                resultDic[key] = (m.value as! [String]).joined(separator: ",")
+            }
+            
+            
+        }
+        return resultDic
+    }
+    
+    /// 該当エンティティはをjson文字列に変換
+    ///
+    /// - Returns: json文字列
+    func getParamJsonString()->String
+    {
+        return String.init(data: FFCore.jsonSerialization(jsonObjec: self.getParamToDictionary()) ?? Data() , encoding: .utf8) ?? ""
+    }
+    
+}
+
+enum YTRequestType:String
+{
+    case videos = "videos"
+    case playLists = "playlists"
+}
+
+class YTVideosRequest:YTEntityBase
+{
+    var apiType:YTRequestType = .videos
+    var id:[String] = []
+    var key:String = GoogleApiKey
+    var part:[String] = ["snippet"]
+    
+    func getFullUrl() -> String
+    {
+        return GoogleApiBaseUrl + self.apiType.rawValue + "?" + self.getParamToDictionary().toUrlParam()
+    }
+}
